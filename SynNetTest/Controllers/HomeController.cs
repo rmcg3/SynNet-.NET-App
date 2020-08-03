04e8dfb11ef7975579e303ec76b4c0e6c9afd9ef
rmcgarry3 <ryanmcgarry3@gmail.com>
2020-08-03 15:53:50 -0400

    add all files

---
+++ SynNetTest/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Syn.WordNet;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SynNetTest.Models;
+
+namespace SynNetTest.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var home = new HomeModel()
+            {
+                directory = Directory.GetCurrentDirectory(),
+                wordNet = new WordNetEngine(),
+            };
+            return View(home);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SynNetTest/Models/HomeModel.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Syn.WordNet;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SynNetTest.Models
+{
+
+
+    public class HomeModel
+    {
+        public string directory { get; set; }
+        public WordNetEngine wordNet { get; set; }
+        [FromForm]
+        public string word { get; set; }
+        public List<SynSet> synSetList { get; set; }
+
+        //load words to access
+        public string Load()
+        {
+            wordNet.AddDataSource(new StreamReader(Path.Combine(directory, "data.adj")), PartOfSpeech.Adjective);
+            wordNet.AddDataSource(new StreamReader(Path.Combine(directory, "data.adv")), PartOfSpeech.Adverb);
+            wordNet.AddDataSource(new StreamReader(Path.Combine(directory, "data.noun")), PartOfSpeech.Noun);
+            wordNet.AddDataSource(new StreamReader(Path.Combine(directory, "data.verb")), PartOfSpeech.Verb);
+
+            wordNet.AddIndexSource(new StreamReader(Path.Combine(directory, "index.adj")), PartOfSpeech.Adjective);
+            wordNet.AddIndexSource(new StreamReader(Path.Combine(directory, "index.adv")), PartOfSpeech.Adverb);
+            wordNet.AddIndexSource(new StreamReader(Path.Combine(directory, "index.noun")), PartOfSpeech.Noun);
+            wordNet.AddIndexSource(new StreamReader(Path.Combine(directory, "index.verb")), PartOfSpeech.Verb);
+
+
+            wordNet.Load();
+            return "Database Loaded Successfully";
+
+        }
+    }
+}
+
